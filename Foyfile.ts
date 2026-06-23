054615621ab59dab890a66600450806df7708984
zaaack <zaaack@qq.com>
2026-06-23 12:59:35 +0800

    deploy task 添加确认提示，手动输入后等待用户确认继续

--- Foyfile.ts
+++ Foyfile.ts
@@ -1,5 +1,6 @@
 import { task, logger, fs } from "foy";
 import { robocopy, runInMiniPC, delInMiniPC } from "./tasks/utils";
+import * as readline from "readline";
 
 task("deploy", async (ctx) => {
     await ctx.exec("pnpm install", { cwd: "server" });
@@ -22,8 +23,23 @@ task("deploy", async (ctx) => {
     await robocopy("server", dist, files);
 
     await runInMiniPC(
-        "cd C:/Users/z/server/joplin-server && pnpm i -P thirty-two",
+        "cd C:/Users/z/server/joplin-server,
     );
+    // 手动输入  pnpm i -P thirty-two  node-os-utils
+
+    // 等待用户确认继续
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    const answer = await new Promise<string>((resolve) => {
+        rl.question("继续部署？(y/回车=继续, 其他=取消): ", (ans) => {
+            rl.close();
+            resolve(ans);
+        });
+    });
+    if (answer && answer !== "y") {
+        logger.info("部署已取消");
+        return;
+    }
+
     await runInMiniPC(
         "cd C:/Users/z/server/joplin-server && node set-wal-mode.js db-prod.sqlite",
     );
